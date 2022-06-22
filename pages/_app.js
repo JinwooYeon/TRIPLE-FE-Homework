5417a164050e76d12a434a30816250fc55ef7c6c
연진우 <2dwd10@gmail.com>
2022-06-23 02:02:43 +0900

    Feat : :sparkles: tab information 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* 탭 정보 */}
+      <Head>
+        <title>트리플: 여행의 모든 것, 트리플로 한번에</title>
+        <meta name="description" content="TRIPLE-FE-Homework" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
